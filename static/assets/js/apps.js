a2955d41e1c7f9a4d3c9aee409b14f0e45a3d102
ubgultra <155318917+ubgultra@users.noreply.github.com>
2024-11-20 09:08:42 -0500

    remove

--- static/assets/js/apps.js
+++ static/assets/js/apps.js
@@ -51,29 +51,14 @@ function addApp(name, imageUrl, onClickFunction, width, height) {
 }
 
 var apps = {
-  Google: { imageUrl: "/assets/img/google-icon.webp", onClick: google },
+  Unblocked Google: { imageUrl: "/assets/img/google-icon.webp", onClick: google },
   ChatGPT: { imageUrl: "/assets/img/gpt.webp", onClick: gpt },
-  'Gemini': { imageUrl: "/assets/img/gemini.webp", onClick: gemini },
-  Discord: { imageUrl: "/assets/img/discord.webp", onClick: discord },
-  Twitter: { imageUrl: "/assets/img/twitter-x.webp", onClick: twitter },
-  Reddit: { imageUrl: "/assets/img/reddit.webp", onClick: reddit },
-  Pinterest: { imageUrl: "/assets/img/pinterest.webp", onClick: pin },
   'CoolMath': { imageUrl: "/assets/img/coolmathgames.webp", onClick: cmg },
   CrazyGames: { imageUrl: "/assets/img/crazygames.webp", onClick: crazygms },
   'Y8 Games': { imageUrl: "/assets/img/y8.webp", onClick: y8 },
   'Neal.fun': { imageUrl: "/assets/img/neal.webp", onClick: nf},
-  'GeForce Now': { imageUrl: "/assets/img/geforcenow.webp", onClick: geforce },
-  'Now.GG': { imageUrl: "/assets/img/nowgg.webp", onClick: ng },
   'Chess.com': { imageUrl: "/assets/img/chess.webp", onClick: chess },
   'Symbolab Math Solver': { imageUrl: "/assets/img/symbolab.webp", onClick: symbolab, height: 130 },
-  YouTube: { imageUrl: "/assets/img/youtube.webp", onClick: yt },
-  Netflix: { imageUrl: "/assets/img/netflix.webp", onClick: netflix },
-  SFlix: { imageUrl: "/assets/img/sflix.webp", onClick: sFlix },
-  Twitch: { imageUrl: "/assets/img/twitch.webp", onClick: twitch },
-  TikTok: { imageUrl: "/assets/img/tiktok.webp", onClick: tt },
-  Spotify: { imageUrl: "/assets/img/spotify.webp", onClick: spotify },
-  GitHub: { imageUrl: "/assets/img/github.webp", onClick: github },
-  'Visual Studio Code': { imageUrl: "/assets/img/vscode.webp", onClick: vscode, height: 130 },
 
 
 };
