df72479ad2d50e559c9a6db102ce7482a1bd9f0c
ubgultra <155318917+ubgultra@users.noreply.github.com>
2024-11-22 22:30:27 -0500

    Update ag.js

--- static/assets/js/ag.js
+++ static/assets/js/ag.js
@@ -221,8 +221,42 @@ function cookieClicker() {
 }
 
 function rBowl() {
-  openAg('https://ubgultra.vercel.app/assets/gms/retrobowl.html');
-}
+  document.title = 'Doge | V4';
+  var cloakcheckScript = document.createElement("script");
+  cloakcheckScript.src = "/assets/js/cloak.js";
+  document.head.appendChild(cloakcheckScript);
+  var functionsScript = document.createElement("script");
+  functionsScript.src = "/assets/js/functions.js";
+  document.head.appendChild(functionsScript);
+  document.body.innerHTML = `
+  <script src="/assets/js/index.js"></script>
+  <script src="/uv/uv.bundle.js"></script>
+  <script src="/uv/uv.bundle.js"></script>
+  <script src="/uv/uv.config.js"></script>
+  <script src="/assets/js/ag.js"></script>
+  <script>
+    window.navigator.serviceWorker
+      .register("/sw.js", {
+        scope: __uv$config.prefix,
+      });
+  </script>
+  <iframe src="/assets/rb/index.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%; overflow: hidden;"></iframe>
+`;
+  function getRandomThreeDigitNumber() {
+    return Math.floor(Math.random() * 900) + 100;
+  }
+  function getRandomAlphanumericString(length) {
+    const characters = 'abcdefghijklmnopqrstuvw0123456789012345';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  }
+  var randomAlphanumericString = getRandomAlphanumericString(15);
+  var url = '/temp?draft=' + randomAlphanumericString;
+  var title = 'Google Docs';
+  history.pushState({}, title, url);
 
 function krunker() {
   openAg('https://krunker.io');
